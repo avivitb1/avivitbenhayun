d5e27765ce74eb826ac699073b899da6cea7d8bd
avivitb <avivitb@post.bgu.ac.il>
2022-05-20 17:43:00 +0300

    updateTargil2

--- static/JShomepage.js
+++ static/JShomepage.js
@@ -1,22 +1,25 @@
    
-      function msgprint() {  
-        alert("You are Successfully Called the JavaScript function");  
-     }  
-     function myFunction() {
-       document.getElementById("demo").innerHTML = "Paragraph changed.";
-     }
-     function exampleFunction() {
-        alert('You triggered an alert!');
-    }
-    window.onload = function(){
-
-      const activePage = window.location.pathname;
-      console.log(activePage);
-      
-       document.querySelectorAll('a').forEach(link => {    
-        if(link.href.includes(`${activePage}`)){
-          link.classList.add('active');
-        }
-      });
-      
+     function ButtonFunction() {
+      var txt;
+      if (confirm("תאשרו שאתם מעל גיל 16")) {
+        txt = "You pressed OK!";
+      } else {
+        txt = "You pressed Cancel!";
       }
+      document.getElementById("demo").innerHTML = txt;
+    }
+    
+      window.onload = function(){
+
+        const homepage = window.location.pathname;
+        console.log(homepage);
+        
+         document.querySelectorAll('a').forEach(link => {    
+          if(link.href.includes(`${homepage}`)){
+            link.classList.add('markURL');
+          }
+        });
+        
+        }
+        
+    
